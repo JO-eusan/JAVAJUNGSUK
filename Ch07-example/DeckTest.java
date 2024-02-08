59fc424e191e4a85107639c4899966f162fc2c9b
JO-eusan <goodsan0330>
2024-02-08 16:24:49 +0900

    2024-02-08

--- Ch07-example/DeckTest.java
+++ Ch07-example/DeckTest.java
@@ -5,7 +5,7 @@ class DeckTest
     public static void main(String[] args)
     {
         Deck d = new Deck(); // 카드 한 벌을 만든다.
-        Card c = d.pick(0); // 섞기 전에 제일 위의 카드를 뽑음
+        Card2 c = d.pick(0); // 섞기 전에 제일 위의 카드를 뽑음
         System.out.println(c); // System.out.println(c.toString())과 같다.
 
         d.shuffle(); // 카드를 섞음
@@ -17,24 +17,24 @@ class DeckTest
 class Deck
 {
     final int CARD_NUM = 52; // 카드의 개수
-    Card cardArr[] = new Card[CARD_NUM]; // Card객체 배열
+    Card2 cardArr[] = new Card2[CARD_NUM]; // Card객체 배열
 
     // 생성자
     Deck() // Deck의 카드를 초기화
     {
         int i=0; 
 
-        for(int k=Card.KIND_MAX; k>0; k--)
-            for(int n=0; n<Card.NUM_MAX; n++)
-                cardArr[i++] = new Card(k, n+1);
+        for(int k=Card2.KIND_MAX; k>0; k--)
+            for(int n=0; n<Card2.NUM_MAX; n++)
+                cardArr[i++] = new Card2(k, n+1);
     }
 
-    Card pick(int index) // 지정된 위치(index)에 있는 카드 하나를 꺼내서 반환
+    Card2 pick(int index) // 지정된 위치(index)에 있는 카드 하나를 꺼내서 반환
     {
          return cardArr[index];
     }
 
-    Card pick() // Deck에서 카드 하나를 선택
+    Card2 pick() // Deck에서 카드 하나를 선택
     {
         int index = (int)(Math.random() * CARD_NUM);
         return pick(index);
@@ -46,7 +46,7 @@ class Deck
         {
             int r = (int)(Math.random() * CARD_NUM);
 
-            Card temp = cardArr[i];
+            Card2 temp = cardArr[i];
             cardArr[i] = cardArr[r];
             cardArr[r] = temp;
         }
@@ -55,7 +55,7 @@ class Deck
     
 }
 
-class Card
+class Card2
 {
     static final int KIND_MAX = 4; // 카드 무늬의 수
     static final int NUM_MAX = 13; // 무늬별 카드 수
@@ -69,11 +69,11 @@ class Card
     int number;
 
     // 생성자
-    Card()
+    Card2()
     {
         this(SPADE, 1); // 기본값
     }
-    Card(int kind, int number)
+    Card2(int kind, int number)
     {
         this.kind = kind;
         this.number = number;
